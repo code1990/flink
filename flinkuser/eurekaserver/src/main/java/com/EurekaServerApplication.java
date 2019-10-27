8f142da08206a99fdda769936890a0525fb49748
code1990 <s1332177151@sina.com>
2019-10-27 17:40:53 +0800

    eureka-server
    eureka-server

---
+++ flinkuser/eurekaserver/src/main/java/com/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @program: flinkuser
+ * @Date: 2019-10-27 17:32
+ * @Author: code1990
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
