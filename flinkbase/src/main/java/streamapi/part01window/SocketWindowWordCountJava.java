fcc08b3f5fd2e9e8a4ca974cfcb23fd6714d4d24
code1990 <s1332177151@sina.com>
2019-11-04 21:39:12 +0800

    flinkbase
    flinkbase

---
+++ flinkbase/src/main/java/streamapi/part01window/SocketWindowWordCountJava.java
@@ -0,0 +1,13 @@
+package streamapi.part01window;
+
+/**
+ * @program: flinkbase
+ * @Date: 2019-11-04 21:18
+ * @Author: code1990
+ * @Description:
+ */
+public class SocketWindowWordCountJava {
+    public static void main(String[] args) {
+        String s = new String("");
+    }
+}
