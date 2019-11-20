a4356c4fa9a89405b13669139115f829902910fe
code1990 <s1332177151@sina.com>
2019-11-20 23:10:31 +0800

    test_ok
    test_ok

--- flinkbase/streaming/src/main/java/part02datasource/StreamingFromCollectionJava.java
+++ flinkbase/streaming/src/main/java/part02datasource/StreamingFromCollectionJava.java
@@ -1,10 +1,40 @@
 package part02datasource;
 
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.ArrayList;
+
 /**
  * @program: flinkbase
  * @Date: 2019-11-17 20:24
  * @Author: code1990
- * @Description:
+ * @Description: 读取集合的数据
  */
 public class StreamingFromCollectionJava {
+    public static void main(String[] args) throws Exception {
+        //获取运行时环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        //声明一个数组
+        ArrayList<Integer> data = new ArrayList<>();
+        data.add(1);
+        data.add(11);
+        data.add(111);
+        //从集合中读取数据
+        DataStreamSource<Integer> collectionData = env.fromCollection(data);
+        //通过Map对数据进行处理
+        DataStream<Integer> num = collectionData.map(new MapFunction<Integer, Integer>() {
+            @Override
+            public Integer map(Integer integer) throws Exception {
+                //遍历每一个数字+1
+                return integer+1;
+            }
+        });
+        //直接打印数据 并设置并行度
+        num.print().setParallelism(1);
+        //必须执行 否则无结果
+        env.execute("collection sources");
+    }
 }
--- flinkbase/streaming/src/main/java/part02datasource/StreamingFromCollectionScala.scala
+++ flinkbase/streaming/src/main/java/part02datasource/StreamingFromCollectionScala.scala
@@ -1,6 +1,25 @@
 package part02datasource
 
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
 
 object StreamingFromCollectionScala {
+
+  def main(args: Array[String]): Unit = {
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    //隐式转换
+    import org.apache.flink.api.scala._
+
+    val data = List(1,11,111)
+
+    val text = env.fromCollection(data)
+
+    val num = text.map(_+1)
+
+    num.print().setParallelism(1)
+
+    env.execute("StreamingFromCollectionScala")
+  }
 
 }
